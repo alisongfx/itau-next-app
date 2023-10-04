c8b3749858bf9a61a5c9065efdef3d7e581625a6
Alison <alisongfx21@gmail.com>
2023-10-03 21:23:53 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+import Logo from "@/assets/logo.svg";
+import ItemMenu from "./ItemMenu";
+import Iconuser from '@/assets/icon-user.svg'
+import { Search } from "./Search";
+import { Container } from "./Container";
+
+export function Header() {
+  return (
+    <header className="relative w-full flex items-center h-20 bg-primary-orange">
+      <div className="absolute top-0 right-0 bg-primary-blue h-full w-[19%] z-0"></div>
+        <Container>
+        <div className="flex flex-1 items-center justify-between">
+          <div className="flex items-center gap-14">
+            <Image src={Logo} alt="Logo" />
+            <ul className="flex items-center gap-12">
+              <li>
+                <ItemMenu name="Para você"/>
+              </li>
+              <li>
+                <ItemMenu name="Para empresas"/>
+              </li>
+              <li>
+                <ItemMenu name="Serviços"/>
+              </li>
+              <li>
+                <ItemMenu name="Ajuda"/>
+              </li>
+            </ul>
+          </div>
+
+        <Search />
+        </div>
+        <button className="flex items-center gap-4 bg-primary-blue h-20 pl-10 z-10">
+          <Image src={Iconuser} alt="icon user"/>
+          <span className="text-white font-bold">Acessar conta</span></button>
+      </Container>
+    </header>
+  );
+}
